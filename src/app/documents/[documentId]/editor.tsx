a68b1cf293767cb60a15827df17908b39779aa08
bret <bretai@LCARS-MX-23>
2024-12-10 18:11:21 +0300

    Add Tiptap extensions && Editor store

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,12 +1,48 @@
 "use client";
 
+import Image from "@tiptap/extension-image";
+import Table from "@tiptap/extension-table";
+import TableCell from "@tiptap/extension-table-cell";
+import TableHeader from "@tiptap/extension-table-header";
+import TableRow from "@tiptap/extension-table-row";
+import TaskItem from "@tiptap/extension-task-item";
+import TaskList from "@tiptap/extension-task-list";
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import ImageResize from "tiptap-extension-resize-image";
+
+import { useEditorStore } from "@/store/use-editor-store";
 
 export interface IEditorProps {}
 
 export default function Editor(props: IEditorProps) {
+  const { setEditor } = useEditorStore();
+
   const editor = useEditor({
+    onCreate({ editor }) {
+      setEditor(editor);
+    },
+    onDestroy() {
+      setEditor(null);
+    },
+    onUpdate({ editor }) {
+      setEditor(editor);
+    },
+    onSelectionUpdate({ editor }) {
+      setEditor(editor);
+    },
+    onTransaction({ editor }) {
+      setEditor(editor);
+    },
+    onFocus({ editor }) {
+      setEditor(editor);
+    },
+    onBlur({ editor }) {
+      setEditor(editor);
+    },
+    onContentError({ editor }) {
+      setEditor(editor);
+    },
     editorProps: {
       attributes: {
         style: "padding-left: 56px; padding-right: 56px;",
@@ -14,8 +50,33 @@ export default function Editor(props: IEditorProps) {
           "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
       },
     },
-    extensions: [StarterKit],
-    content: "<p>Hello world! 🌍",
+    extensions: [
+      StarterKit,
+      Image,
+      ImageResize,
+      TableRow,
+      TableHeader,
+      TableCell,
+      Table,
+      TaskItem.configure({ nested: true }),
+      TaskList,
+    ],
+    content: `
+      <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th colspan="3">Description</th>
+            </tr>
+            <tr>
+              <td>Cyndi Lauper</td>
+              <td>Singer</td>
+              <td>Songwriter</td>
+              <td>Actress</td>
+            </tr>
+          </tbody>
+        </table>
+    `,
   });
 
   return (
--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,4 +1,5 @@
 import Editor from "./editor";
+import Toolbar from "./toolbar";
 
 export interface IDocumentIdPageProps {
   params: Promise<{ documentId: string }>;
@@ -8,6 +9,7 @@ export default async function DocumentIdPage({ params }: IDocumentIdPageProps) {
   const { documentId } = await params;
   return (
     <div className="min-h-screen bg-[#FAFBFD]">
+      <Toolbar />
       <Editor />
     </div>
   );
